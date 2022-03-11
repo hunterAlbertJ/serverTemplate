bb5fc78eb09eccc70a4b312bf6749aa1ae4b3be3
hunterAlbertJ <albert.hunter13@gmail.com>
2022-03-11 14:57:19 -0600

    template finished for real

--- routes.js
+++ routes.js
@@ -1,11 +1,11 @@
 require("dotenv").config(); 
 const { Pool } =  require("pg");
 const pool = new Pool({
-    database: process.env.DATABASE_URL,
-    // connectionString: process.env.DATABASE_URL,
-    // ssl:{
-    //   rejectUnauthorized:false
-    // },
+    // database: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
+    ssl:{
+      rejectUnauthorized:false
+    },
 });
 
 function retrieve(res, index) {
